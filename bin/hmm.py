0760d3a33b2ec759946a07062f44131fc5ec8a1e
AdrianForsythe <adrian.e.forsythe@gmail.com>
2023-10-01 12:52:51 +0200

    flexdashboard, blast/hmmer, phylogeny

--- bin/hmm.py
+++ bin/hmm.py
@@ -1,29 +1,80 @@
+import argparse
+import pandas as pd
 from Bio import SeqIO
 
-# Path to the HMMER output file
-hmmer_output_file = "/home/adrian/Systematics/output.txt"
+parser = argparse.ArgumentParser(
+    description="Parse HMMER output and extract gene sequences from full starship sequence"
+)
+parser.add_argument(
+    "-i",
+    "--hmmer_output_file",
+    dest="hmmer_output_file",
+    help="Path to output file from HMMER search, in `domtblout` format",
+    required=True,
+)
+parser.add_argument(
+    "-q",
+    "--ship_seq",
+    dest="ship_seq",
+    help="Path to query sequence FASTA to extract gene sequences from",
+    required=False,
+)
+args = parser.parse_args()
 
-# Path to the query sequence file in FASTA format
-query_sequence_file = "/home/adrian/Systematics/Starship_Database/Starships/genes/cap_tyr/YRsuperfamRefs.faa.split/YRsuperfamRefs.part_aaoarx1_319010.faa"
+
+# Parse the HMMER results
+def parse_hmmer(hmmer_output_file):
+    with open(hmmer_output_file, "r") as hmmer_output:
+        for line in hmmer_output:
+            if line.startswith("#"):
+                continue
+            fields = line.strip().split()
+            hit_name = fields[0]
+            length = int(fields[2])
+            query_name = fields[3]
+            eval = fields[11]
+            score = fields[13]
+
+            df = pd.DataFrame(
+                {
+                    "query_ID": query_name,
+                    "hit_IDs": hit_name,
+                    "hit_length": length,
+                    "bitscore": score,
+                    "eval": eval,
+                },
+                index=[0],
+            )
+            df.to_csv("tmp/hmmer-parsed.csv")
+
+
+parse_hmmer(args.hmmer_output_file)
 
 # Create a dictionary to store sequences
 sequences = {}
 
-# Parse the HMMER results
-with open(hmmer_output_file, "r") as hmmer_output:
-    for line in hmmer_output:
-        if line.startswith("#"):
-            continue
-        fields = line.strip().split()
-        hit_name = fields[3]
-        start = int(fields[19])
-        end = int(fields[20])
-        # Load the query sequence
-        query_sequences = SeqIO.to_dict(SeqIO.parse(query_sequence_file, "fasta"))
-        if hit_name in query_sequences:
-            hit_sequence = query_sequences[hit_name]
-            gene_sequence = hit_sequence[start - 1 : end]  # Adjust for 0-based indexing
-            sequences[hit_name] = gene_sequence
-            print(sequences[hit_name])
 
-# Now, 'sequences' contains the extracted gene sequences
+def extract_hmmer(hmmer_output_file, ship_seq):
+    with open(hmmer_output_file, "r") as hmmer_output:
+        for line in hmmer_output:
+            if line.startswith("#"):
+                continue
+            fields = line.strip().split()
+            hit_name = fields[0]
+            query_name = fields[3]
+            start = int(fields[19])
+            end = int(fields[20])
+            # Load the query sequence
+            query_sequences = SeqIO.to_dict(SeqIO.parse(ship_seq, "fasta"))
+            if query_name in query_sequences:
+                hit_sequence = query_sequences[query_name]
+                gene_sequence = hit_sequence[
+                    start - 1 : end
+                ]  # Adjust for 0-based indexing
+                sequences[query_name] = gene_sequence
+                # TODO: SeqIO write fasta
+                # print(sequences[query_name])
+
+
+if args.ship_seq:
+    extract_hmmer(args.hmmer_output_file, args.ship_seq)
