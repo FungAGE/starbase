639f5fd6af5e307df92d7f6b614f576f71eef9fd
AdrianForsythe <adrian.e.forsythe@gmail.com>
2023-10-23 17:43:05 +0200

    updated blast workflow

--- dev/03_deploy.R
+++ dev/03_deploy.R
@@ -32,7 +32,7 @@ remotes::install_local("starbase_0.0.0.9000.tar.gz")
 ## If you want to deploy via a generic Dockerfile
 # golem::add_dockerfile_with_renv()
 
-## If you want to deploy to ShinyProxy
+# If you want to deploy to ShinyProxy
 # golem::add_dockerfile_with_renv_shinyproxy()
 
 # Deploy to Posit Connect or ShinyApps.io
