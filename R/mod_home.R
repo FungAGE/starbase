7c3fea5c99709bdc4f0ebb7e52c2eb5cc4826147
AdrianForsythe <adrian.e.forsythe@gmail.com>
2023-10-25 17:19:14 +0200

    added template for synteny view

--- R/mod_home.R
+++ R/mod_home.R
@@ -7,20 +7,20 @@
 #' @noRd 
 #'
 #' @importFrom shiny NS tagList 
+#' @importFrom shinyjs disabled
 #' 
 
 mod_home_ui <- function(id){
   ns <- NS(id)
   dashboardBody(
+      div(fluidRow(style="padding-bottom: 15px;",column(width=1,img(src="www/favicon.png",width="100%")),
+        column(width=3,h2("starbase: Database and tools for exploring large eukaryotic transposable elements in Fungi")))),
     fluidRow(
-      box(icon=img(src="www/favicon.ico"),
-        p("starbase: Database and tools for exploring large eukaryotic transposable elements in Fungi")),
-      box(div(
-        img(src = "img/starship-summary.png",
-            width = "75%",
-            style = "background-color: white;"
-          )
-        )
+      box(title="This is the development version of starbase",status="warning",
+        disabled(
+        checkboxGroupInput(ns("functions"),label="Working Functions:",
+          choices=c("BLAST/HMMER searches","Synteny/Genome Browser","Starship Catalogue","Running Starfish","Submission of new Ships","Editing of Database entries"),
+          selected="BLAST/HMMER searches"))
       )
     )
   )
