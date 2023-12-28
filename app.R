ac820f4864ebc50112bbe2cffad3d7c11edd1fd5
Adrian Forsythe <adrian.forsythe@ebc.uu.se>
2023-12-28 10:43:52 -0500

    Update app.R

--- app.R
+++ app.R
@@ -7,12 +7,12 @@ pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 # Set options here
 # TRUE = production mode, FALSE = development mode
 options(golem.app.prod = FALSE) 
-
 # options(shiny.testmode = TRUE)
 options(shiny.autoreload = TRUE)
 
 # Comment this if you don't want the app to be served on a random port
 # options(shiny.port = httpuv::randomPort())
+options('shiny.port'=3838,shiny.host='127.0.0.1')
 
 # for bioc repos
 options(repos = BiocManager::repositories())
@@ -21,4 +21,4 @@ options(repos = BiocManager::repositories())
 options(shiny.reactlog=TRUE)
 
 # Run the application
-starbase::run_app() # add parameters here (if any)
+golex::run_app()
