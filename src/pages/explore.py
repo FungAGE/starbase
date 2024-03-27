9a48f3e1140eab2273aa168fe8eb0bda1a8e62a4
AdrianForsythe <adrian.e.forsythe@gmail.com>
2024-03-27 17:13:02 +0100

    preparing for prelim deploy

--- src/pages/explore.py
+++ src/pages/explore.py
@@ -1,105 +1,190 @@
 import dash
-import dash_bootstrap_components as dbc
 from dash import dash_table, dcc, html, callback
-from dash.dependencies import Output, Input, State
+from dash.dependencies import Output, Input
+import dash_bootstrap_components as dbc
 
-import dash_ag_grid as dag
-import base64
 import pandas as pd
 import plotly.express as px
 
-from textwrap import dedent as d
-
 dash.register_page(__name__)
 
-df = pd.read_csv('src/assets/joined_ships.csv')
-# df = df[["starshipID","starship_family","starship_navis","starship_haplotype","genus",'species']]
+df = pd.read_csv("src/assets/joined_ships.csv")
+df_sub = df[
+    [
+        "starshipID",
+        "starship_family",
+        "starship_navis",
+        "starship_haplotype",
+        "genus",
+        "species",
+    ]
+]
 
-ship_agg = df.groupby(["starship_family"]).starshipID.agg(['count', 'nunique', lambda x: x.size - x.nunique()])
+ship_count = df[["starshipID"]].nunique()
+species = df["genus"] + "-" + df["species"]
+species_count = species.nunique()
+
+ship_agg = df.groupby(["starship_family", "starship_navis"]).starshipID.agg(
+    ["count", "nunique", lambda x: x.size - x.nunique()]
+)
 ship_agg = ship_agg.reset_index()
 
-tax_agg = df.groupby(["order"]).starshipID.agg(['count', 'nunique', lambda x: x.size - x.nunique()])
+tax_agg = df.groupby(["order", "family"]).starshipID.agg(
+    ["count", "nunique", lambda x: x.size - x.nunique()]
+)
 tax_agg = tax_agg.reset_index()
 
-styles = {
-    'pre': {
-        'border': 'thin lightgrey solid',
-        'overflowX': 'scroll'
-    }
-}
+styles = {"pre": {"border": "thin lightgrey solid", "overflowX": "scroll"}}
 
-layout = html.Div([
-        html.Div([
-            dcc.Graph(id='pie-chart1', config={"displayModeBar": False}),
-            dcc.Graph(id='pie-chart2', config={"displayModeBar": False}),
-        ],
-            style={'display':'inline-block','width': '50%'}),
-        html.Div([
-            html.H3("Table for metadata of all Starships in starbase"),
-            dash_table.DataTable(
-                id='table',
-                columns=[
-                    {"name": i, "id": i, "deletable": False, "selectable": True} for i in df.columns
-                ],
-                data=df.to_dict('records'),
-                editable=False,
-                filter_action="native",
-                sort_action="native",
-                sort_mode="multi",
-                column_selectable="single",
-                row_selectable="multi",
-                row_deletable=True,
-                selected_columns=[],
-                selected_rows=[],
-                page_action="native",
-                page_current= 0,
-                page_size= 10,
-            ),html.Div(id='table-container')
-        ],style={'display':'inline-block','width': '50%'},)
-    ])
-
-@callback(
-    [Output('pie-chart1', 'figure'),
-     Output('pie-chart2', 'figure'),
-     Output('table', 'selected_rows')],
-    [Input('table', 'selected_rows')]
+layout = html.Div(
+    [
+        html.H1("Explore Starship Diversity"),
+        dbc.Row(
+            [
+                dbc.Col(
+                    dbc.Card(
+                        dbc.CardBody(
+                            [
+                                html.H4(
+                                    html.P(
+                                        [
+                                            "Total number of Starships in ",
+                                            html.Span(
+                                                "starbase",
+                                                className="logo-text",
+                                            ),
+                                        ]
+                                    ),
+                                    className="card-title",
+                                ),
+                                html.P(ship_count, className="card-text"),
+                            ]
+                        ),
+                        style={"font-size": "26px"},
+                        color="primary",
+                        inverse=True,
+                    ),
+                    width=2,
+                ),
+                dbc.Col(
+                    dbc.Card(
+                        dbc.CardBody(
+                            [
+                                html.H4(
+                                    "Fungal species with Starships",
+                                    className="card-title",
+                                ),
+                                html.P(species_count, className="card-text"),
+                            ]
+                        ),
+                        style={"font-size": "26px"},
+                        color="secondary",
+                        inverse=True,
+                    ),
+                    width=2,
+                ),
+            ]
+        ),
+        html.Div(
+            [
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            dcc.Graph(id="pie-chart2", config={"displayModeBar": False})
+                        ),
+                        dbc.Col(
+                            dcc.Graph(id="pie-chart1", config={"displayModeBar": False})
+                        ),
+                    ]
+                )
+            ],
+            style={"display": "inline-block", "width": "85%"},
+        ),
+        html.Div(
+            [
+                html.H3("Table for metadata of all Starships in starbase"),
+                dash_table.DataTable(
+                    id="table",
+                    columns=[
+                        {"name": i, "id": i, "deletable": False, "selectable": True}
+                        for i in df_sub.columns
+                    ],
+                    data=df_sub.to_dict("records"),
+                    editable=False,
+                    filter_action="native",
+                    sort_action="native",
+                    sort_mode="multi",
+                    column_selectable="single",
+                    row_selectable="multi",
+                    row_deletable=False,
+                    selected_columns=[],
+                    selected_rows=[],
+                    page_action="native",
+                    page_current=0,
+                    page_size=25,
+                ),
+                html.Div(id="table-container"),
+            ],
+            style={"display": "inline-block", "width": "85%"},
+        ),
+    ]
 )
 
+
+@callback(
+    [
+        Output("pie-chart1", "figure"),
+        Output("pie-chart2", "figure"),
+        Output("table", "selected_rows"),
+    ],
+    [Input("table", "selected_rows")],
+)
 def update_plots(selected_rows):
     if selected_rows is None or len(selected_rows) == 0:
-        ship_agg_filt=ship_agg
-        tax_agg_filt=tax_agg
+        ship_agg_filt = ship_agg
+        tax_agg_filt = tax_agg
     else:
         # TODO: filter by specific column, not index
         ship_agg_filt = ship_agg.iloc[selected_rows]
         tax_agg_filt = tax_agg.iloc[selected_rows]
 
-    ship_pie = px.pie(ship_agg_filt, values="count", names="starship_family",title="Starships in starbase by captain family")
-    tax_pie = px.pie(tax_agg_filt, values="count", names="order",title="Starships in starbase by Order")
-        
+    ship_pie = px.sunburst(
+        ship_agg_filt,
+        path=["starship_family", "starship_navis"],
+        values="count",
+        title="Starships in starbase by captain family",
+    )
+    tax_pie = px.sunburst(
+        tax_agg_filt,
+        path=["order", "family"],
+        values="count",
+        title="Starships in starbase by Order",
+    )
+
     return ship_pie, tax_pie, selected_rows
-    
+
 
 # this callback defines 3 figures
 # as a function of the intersection of their 3 selections
-@callback(
-    Output("g1", "figure"),
-    Output("g2", "figure"),
-    Input("g1", "selectedData"),
-    Input("g2", "selectedData"),
-)
-def callback(selection1, selection2):
-    selectedpoints = df.index
-    for selected_data in [selection1, selection2]:
-        if selected_data and selected_data["points"]:
-            selectedpoints = np.intersect1d(
-                selectedpoints, [p["customdata"] for p in selected_data["points"]]
-            )
+# @callback(
+#     Output("g1", "figure"),
+#     Output("g2", "figure"),
+#     Input("g1", "selectedData"),
+#     Input("g2", "selectedData"),
+# )
+# def callback(selection1, selection2):
+#     selectedpoints = df.index
+#     for selected_data in [selection1, selection2]:
+#         if selected_data and selected_data["points"]:
+#             selectedpoints = np.intersect1d(
+#                 selectedpoints, [p["customdata"] for p in selected_data["points"]]
+#             )
 
-    return [
-        get_figure(df, "Col 1", "Col 2", selectedpoints, selection1),
-        get_figure(df, "Col 3", "Col 4", selectedpoints, selection2)
-    ]
+#     return [
+#         get_figure(df, "Col 1", "Col 2", selectedpoints, selection1),
+#         get_figure(df, "Col 3", "Col 4", selectedpoints, selection2),
+#     ]
+
 
 # layout = html.Div([
 #     dbc.Container([
@@ -141,12 +226,12 @@ def callback(selection1, selection2):
 #         derived_virtual_selected_rows = []
 
 #     dff = df if rows is None else pd.DataFrame(rows)
-    
+
 #     if derived_virtual_selected_rows:
 #         dff_selected = dff.iloc[derived_virtual_selected_rows]
 #     else:
 #         dff_selected = dff
-    
+
 #     graphs = []
 #     for column in ["starship_family"]:
 #         if column in dff_selected:
