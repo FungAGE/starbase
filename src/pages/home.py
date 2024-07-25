9900f8148c2a1b9bdecf95bbdef121a1338fc6de
Adrian Forsythe <adrian.forsythe@ebc.uu.se>
2024-07-25 15:58:10 +0200

    home page with reactive layout, suitable for small screens

--- src/pages/home.py
+++ src/pages/home.py
@@ -5,7 +5,7 @@ from dash import html
 dash.register_page(__name__, title="Home", name="Home", path="/")
 
 
-def mod_home_ui():
+def home_ui():
     working = [
         "Catalogue/Wiki of Starship Metadata",
         "Submission of new Starship sequences",
@@ -13,9 +13,6 @@ def mod_home_ui():
     ]
     working_ul = html.Ul(
         [html.Li(item) for item in working],
-        style={
-            "fontSize": "0.6vw",
-        },
     )
 
     not_working = [
@@ -32,104 +29,92 @@ def mod_home_ui():
     ]
     not_working_ul = html.Ul(
         [html.Li(item) for item in not_working],
-        style={
-            "fontSize": "0.6vw",
-        },
     )
 
-    return html.Div(
-        [
-            dbc.Container(
-                fluid=True,
-                children=[
+    layout = (
+        dbc.Container(
+            fluid=True,
+            children=[
+                dbc.Row(
                     dbc.Col(
-                        width={"size": 6, "offset": 3},
                         children=[
-                            dbc.Row(
-                                html.H1(
-                                    [
-                                        html.Span(
-                                            "starbase",
-                                            className="logo-text",
-                                            style={
-                                                "fontSize": "7vw",
-                                            },
-                                        ),
-                                        html.Br(),
-                                        "A database and toolkit for exploring large eukaryotic transposable elements in Fungi",
-                                    ],
-                                    style={
-                                        "fontSize": "2vw",
-                                        "textAlign": "center",
-                                    },
-                                )
-                            ),
-                            dbc.Row(
+                            html.H1(
                                 [
-                                    dbc.Col(
-                                        [
-                                            dbc.Card(
-                                                [
-                                                    dbc.CardHeader(
-                                                        html.H4(
-                                                            [
-                                                                "What can I currently use ",
-                                                                html.Span(
-                                                                    "starbase",
-                                                                    className="logo-text",
-                                                                ),
-                                                                " for?",
-                                                            ],
-                                                            style={
-                                                                "fontSize": "1vw",
-                                                            },
-                                                        )
-                                                    ),
-                                                    dbc.CardBody([working_ul]),
-                                                ]
-                                            )
-                                        ]
+                                    html.Span(
+                                        "starbase",
+                                        className="logo-text",
                                     ),
-                                    dbc.Col(
-                                        [
-                                            dbc.Card(
-                                                [
-                                                    dbc.CardHeader(
-                                                        html.H4(
-                                                            [
-                                                                "Functions of ",
-                                                                html.Span(
-                                                                    "starbase",
-                                                                    className="logo-text",
-                                                                ),
-                                                                " under active development:",
-                                                            ],
-                                                            style={
-                                                                "fontSize": "1vw",
-                                                            },
-                                                        )
-                                                    ),
-                                                    dbc.CardBody([not_working_ul]),
-                                                ]
-                                            )
-                                        ]
-                                    ),
-                                ]
+                                ],
+                                style={
+                                    "align-items": "center",
+                                    "justify-content": "center",
+                                    "fontSize": "7vw",
+                                    "textAlign": "center",
+                                },
                             ),
-                            dbc.Row(
-                                dbc.Col(
+                            html.H2(
+                                [
+                                    "A database and toolkit for exploring large eukaryotic transposable elements in Fungi",
+                                ],
+                                style={
+                                    "align-items": "center",
+                                    "justify-content": "center",
+                                    "textAlign": "center",
+                                },
+                            ),
+                        ],
+                        width={"size": 10, "offset": 0.5},
+                    ),
+                    justify="center",
+                ),
+                dbc.Row(
+                    dbc.Col(
+                        dbc.Stack(
+                            [
+                                dbc.Card(
+                                    [
+                                        dbc.CardHeader(
+                                            html.H4(
+                                                [
+                                                    "What can I currently use ",
+                                                    html.Span(
+                                                        "starbase",
+                                                        className="logo-text",
+                                                    ),
+                                                    " for?",
+                                                ],
+                                            )
+                                        ),
+                                        dbc.CardBody([working_ul]),
+                                    ]
+                                ),
+                                dbc.Card(
+                                    [
+                                        dbc.CardHeader(
+                                            html.H4(
+                                                [
+                                                    "Functions of ",
+                                                    html.Span(
+                                                        "starbase",
+                                                        className="logo-text",
+                                                    ),
+                                                    " under active development:",
+                                                ],
+                                            )
+                                        ),
+                                        dbc.CardBody([not_working_ul]),
+                                    ]
+                                ),
+                                html.Div(
                                     html.Img(
                                         src="assets/images/starbase-map.png",
                                         width="100%",
                                     )
-                                )
-                            ),
-                            dbc.Row(
+                                ),
                                 dbc.Card(
                                     [
                                         dbc.CardHeader(
                                             [html.H4("Data Availability")],
-                                            style={"fontSize": "2vw"},
                                         ),
                                         dbc.CardBody(
                                             [
@@ -142,7 +127,6 @@ def mod_home_ui():
                                                         ),
                                                         " data on our GitHub repo (currently private). We are currently in the process of migrating to a new back-end, which will provide more options for data export. In the mean time, you can retrieve all Starship sequences, annotations, and more, in a single .zip file (size ~100Mb)",
                                                     ],
-                                                    style={"fontSize": "0.6vw"},
                                                 ),
                                                 html.Div(
                                                     style={"textAlign": "center"},
@@ -157,9 +141,6 @@ def mod_home_ui():
                                                                     ),
                                                                     ".",
                                                                 ],
-                                                                style={
-                                                                    "fontSize": "0.6vw"
-                                                                },
                                                             ),
                                                             id="dl_package",
                                                             color="primary",
@@ -170,16 +151,20 @@ def mod_home_ui():
                                             ]
                                         ),
                                     ]
-                                )
-                            ),
-                        ],
-                        md=5,
-                        align="center",
+                                ),
+                            ],
+                            gap=4,
+                            direction="vertical",
+                        ),
+                        width={"size": 10, "offset": 0.5},
                     ),
-                ],
-            ),
-        ]
+                    justify="center",
+                ),
+            ],
+        ),
     )
 
+    return layout
 
-layout = mod_home_ui
+
+layout = home_ui()
