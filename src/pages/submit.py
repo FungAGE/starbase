04be17261dfd97a164e83ea690038b3276ddc6f2
AdrianForsythe <adrian.e.forsythe@gmail.com>
2024-08-13 12:11:46 +0200

    fixing submit button, adding modal

--- src/pages/submit.py
+++ src/pages/submit.py
@@ -2,7 +2,7 @@ import base64
 
 import dash
 import dash_bootstrap_components as dbc
-from dash.dependencies import Output, Input
+from dash.dependencies import Output, Input, State
 from dash import dcc, html, callback
 import sqlite3
 
@@ -325,6 +325,26 @@ layout = html.Div(
                                     id="submit-ship",
                                     n_clicks=0,
                                 ),
+                                dbc.Modal(
+                                    [
+                                        dbc.ModalHeader(
+                                            dbc.ModalTitle("New Submission")
+                                        ),
+                                        dbc.ModalBody(
+                                            html.Div(id="output-data-upload")
+                                        ),
+                                        dbc.ModalFooter(
+                                            dbc.Button(
+                                                "Close",
+                                                id="close",
+                                                className="ms-auto",
+                                                n_clicks=0,
+                                            )
+                                        ),
+                                    ],
+                                    id="modal",
+                                    is_open=False,
+                                ),
                             ],
                         ),
                     ],
@@ -333,6 +353,7 @@ layout = html.Div(
         )
     ],
 )
+
 
 @callback(Output("loading-output-1", "children"), Input("upload-sequence", "value"))
 def input_triggers_spinner(value):
@@ -453,7 +474,7 @@ def update_anno_upload(anno_content, anno_filename):
 
 
 @callback(
-    Output("output-data-upload", "children"),
+    [Output("modal", "is_open"), Output("output-data-upload", "children")],
     [
         Input("upload-sequence", "contents"),
         Input("upload-sequence", "filename"),
@@ -470,7 +491,9 @@ def update_anno_upload(anno_content, anno_filename):
         Input("shipend", "value"),
         Input("comment", "value"),
         Input("submit-ship", "n_clicks"),
+        Input("close", "n_clicks"),
     ],
+    [State("modal", "is_open")],
 )
 def submit_ship(
     seq_content,
@@ -488,97 +511,108 @@ def submit_ship(
     shipend,
     comment,
     n_clicks,
+    close_modal,
+    is_open,
 ):
-    if n_clicks > 0 and seq_content is not None:
-        try:
-            # Create SQLite database connection
-            conn = sqlite3.connect("database_folder/starbase.sqlite")
-            c = conn.cursor()
+    modal = not is_open
+    message = None
+    if n_clicks > 0:
+        if seq_content is None or len(seq_content) == 0:
+            return "No fasta file uploaded"
+        else:
+            try:
+                # Create SQLite database connection
+                conn = sqlite3.connect("database_folder/starbase.sqlite")
+                c = conn.cursor()
 
-            # Check if the table already exists
-            c.execute(
-                "SELECT name FROM sqlite_master WHERE type='table' AND name='submissions_new'"
-            )
-            existing_table = c.fetchone()
+                # Check if the table already exists
+                c.execute(
+                    "SELECT name FROM sqlite_master WHERE type='table' AND name='submissions_new'"
+                )
+                existing_table = c.fetchone()
 
-        except sqlite3.Error as error:
-            print("Error connecting to database.", error)
+            except sqlite3.Error as error:
+                print("Error connecting to database.", error)
 
-        if not existing_table:
-            # Create table for storing form submissions
-            c.execute(
-                """CREATE TABLE submissions_new (
-                seq_contents TEXT NOT NULL,
-                seq_filename TEXT NOT NULL,
-                seq_date TEXT NOT NULL,
-                anno_contents TEXT,
-                anno_filename TEXT,
-                anno_date TEXT,
-                uploader TEXT NOT NULL,
-                evidence TEXT NOT NULL,
-                genus TEXT NOT NULL,
-                species TEXT NOT NULL,
-                hostchr TEXT NOT NULL,
-                shipstart INTEGER NOT NULL,
-                shipend INTEGER NOT NULL,
-                comment TEXT,
-                id	INTEGER NOT NULL,
-                PRIMARY KEY(id AUTOINCREMENT)
-            )"""
-            )
-            conn.commit()
+            if not existing_table:
+                # Create table for storing form submissions
+                c.execute(
+                    """CREATE TABLE submissions_new (
+                    seq_contents TEXT NOT NULL,
+                    seq_filename TEXT NOT NULL,
+                    seq_date TEXT NOT NULL,
+                    anno_contents TEXT,
+                    anno_filename TEXT,
+                    anno_date TEXT,
+                    uploader TEXT NOT NULL,
+                    evidence TEXT NOT NULL,
+                    genus TEXT NOT NULL,
+                    species TEXT NOT NULL,
+                    hostchr TEXT NOT NULL,
+                    shipstart INTEGER NOT NULL,
+                    shipend INTEGER NOT NULL,
+                    comment TEXT,
+                    id	INTEGER NOT NULL,
+                    PRIMARY KEY(id AUTOINCREMENT)
+                )"""
+                )
+                conn.commit()
 
-        try:
-            if seq_content is None or len(seq_content) == 0:
-                return "No fasta file uploaded"
-            else:
+            try:
                 content_type, content_string = seq_content.split(",")
                 seq_decoded = base64.b64decode(content_string).decode("utf-8")
                 seq_datetime_obj = datetime.datetime.fromtimestamp(seq_date)
                 # update_fasta_output(seq_decoded, seq_filename, seq_datetime_obj)
 
-            # ? put annotations into separate SQL submission table?
-            if anno_content is not None and len(anno_content) > 0:
-                content_type, content_string = anno_content.split(",")
-                decoded = base64.b64decode(content_string).decode("utf-8")
-                anno_datetime_obj = datetime.datetime.fromtimestamp(anno_date)
-                # update_gff_output(decoded, anno_filename, anno_datetime_obj)
-            else:
-                anno_contents = ""
-                anno_filename = ""
-                anno_date = ""
+                # ? put annotations into separate SQL submission table?
+                if anno_content is not None and len(anno_content) > 0:
+                    content_type, content_string = anno_content.split(",")
+                    decoded = base64.b64decode(content_string).decode("utf-8")
+                    anno_datetime_obj = datetime.datetime.fromtimestamp(anno_date)
+                    # update_gff_output(decoded, anno_filename, anno_datetime_obj)
+                else:
+                    anno_contents = ""
+                    anno_filename = ""
+                    anno_datetime_obj = ""
 
-            if comment is None:
-                comment = ""
+                if comment is None:
+                    comment = ""
 
-            c.execute(
-                "INSERT INTO submissions_new (seq_contents,seq_filename,seq_date,anno_contents,anno_filename,anno_date,uploader,evidence,genus,species,hostchr,shipstart,shipend,comment) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                (
-                    seq_content,
-                    seq_filename,
-                    seq_datetime_obj,
-                    anno_contents,
-                    anno_filename,
-                    anno_datetime_obj,
-                    uploader,
-                    evidence,
-                    genus,
-                    species,
-                    hostchr,
-                    shipstart,
-                    shipend,
-                    comment,
-                ),
-            )
-            conn.commit()
+                c.execute(
+                    "INSERT INTO submissions_new (seq_contents,seq_filename,seq_date,anno_contents,anno_filename,anno_date,uploader,evidence,genus,species,hostchr,shipstart,shipend,comment) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                    (
+                        seq_content,
+                        seq_filename,
+                        seq_datetime_obj,
+                        anno_contents,
+                        anno_filename,
+                        anno_datetime_obj,
+                        uploader,
+                        evidence,
+                        genus,
+                        species,
+                        hostchr,
+                        shipstart,
+                        shipend,
+                        comment,
+                    ),
+                )
+                conn.commit()
 
-            return html.Div(
-                [html.H5(f"Successfully submitted '{seq_filename}' to starbase")]
-            )
+                modal = True
+                if close_modal:
+                    modal = False
 
-        except sqlite3.Error as error:
-            print("Failed to insert record into SQLite table:", error)
-        finally:
-            c.close()
-            conn.close()
-            print("SQLite connection is closed.")
+                message = html.H5(
+                    f"Successfully submitted '{seq_filename}' to starbase"
+                )
+
+                return modal, message
+
+            except sqlite3.Error as error:
+                print("Failed to insert record into SQLite table:", error)
+
+            finally:
+                c.close()
+                conn.close()
+                print("SQLite connection is closed.")
