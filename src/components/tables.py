a8e171221295b1d1347b23fab9e0e9a4169ce8f3
AdrianForsythe <adrian.e.forsythe@gmail.com>
2024-08-27 17:20:45 +0200

    added new general callback for paper table

--- src/components/tables.py
+++ src/components/tables.py
@@ -7,6 +7,15 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 
 
+def truncate_string(s, length=10):
+    return s if len(s) <= length else s[:length] + "..."
+
+
+# Function to convert URL string to HTML link
+def url_to_link(url, label):
+    return f'<a href="{url}" target="_blank">{label}</a>'
+
+
 def make_ship_table(df, id, columns=None):
     if columns is None:
         # columns = df.columns
@@ -61,24 +70,81 @@ def make_ship_table(df, id, columns=None):
     return table
 
 
-def make_paper_table(data="src/data/papers.csv"):
+def make_paper_table(data):
 
-    df = pd.read_csv(data)
-    sub_df = df[["Title", "Author", "PublicationYear", "DOI", "Url"]].sort_values(
-        by="PublicationYear", ascending=False
+    # df = pd.read_csv(data)
+    # sub_df = df[["Title", "Author", "PublicationYear", "DOI", "Url"]].sort_values(
+    #     by="PublicationYear", ascending=False
+    # )
+
+    df = pd.DataFrame(data)
+
+    df_summary = (
+        df.groupby("Title")
+        .agg(
+            {
+                "familyName": lambda x: ", ".join(sorted(x.unique())),
+                "Author": "first",
+                "PublicationYear": "first",
+                "DOI": "first",
+                "Url": "first",
+            }
+        )
+        .reset_index()
     )
 
-    def truncate_string(s, length=10):
-        return s if len(s) <= length else s[:length] + "..."
+    sub_df = df_summary.sort_values(by="PublicationYear", ascending=False)
 
-    # Function to convert URL string to HTML link
-    def url_to_link(url):
-        return f'<a href="{url}" target="_blank">{url}</a>'
-
-    sub_df["Url"] = sub_df["Url"].apply(url_to_link)
+    sub_df["Title"] = sub_df["Title"].apply(lambda x: truncate_string(x, length=20))
     sub_df["Author"] = sub_df["Author"].apply(lambda x: truncate_string(x, length=20))
+    sub_df["DOI"] = sub_df["DOI"].apply(lambda x: url_to_link(x, label=x))
+    sub_df["Url"] = sub_df["Url"].apply(lambda x: url_to_link(x, label="full text"))
 
-    # TODO: rename columns
+    # rename columns
+    sub_df_columns = [
+        {
+            "name": "Title",
+            "id": "Title",
+            "deletable": False,
+            "selectable": False,
+            "presentation": "markdown",
+        },
+        {
+            "name": "Authors",
+            "id": "Author",
+            "deletable": False,
+            "selectable": False,
+            "presentation": "markdown",
+        },
+        {
+            "name": "Publication Year",
+            "id": "PublicationYear",
+            "deletable": False,
+            "selectable": False,
+            "presentation": "markdown",
+        },
+        {
+            "name": "Starship Families Described",
+            "id": "familyName",
+            "deletable": False,
+            "selectable": False,
+            "presentation": "markdown",
+        },
+        {
+            "name": "DOI",
+            "id": "DOI",
+            "deletable": False,
+            "selectable": False,
+            "presentation": "markdown",
+        },
+        {
+            "name": "Url",
+            "id": "Url",
+            "deletable": False,
+            "selectable": False,
+            "presentation": "markdown",
+        },
+    ]
 
     paper_table = dbc.Card(
         [
@@ -93,16 +159,7 @@ def make_paper_table(data="src/data/papers.csv"):
                     dash_table.DataTable(
                         data=sub_df.to_dict("records"),
                         sort_action="none",
-                        columns=[
-                            {
-                                "name": i,
-                                "id": i,
-                                "deletable": False,
-                                "selectable": False,
-                                "presentation": "markdown",
-                            }
-                            for i in sub_df.columns
-                        ],
+                        columns=sub_df_columns,
                         id="papers-table",
                         markdown_options={"html": True},
                         style_table={
@@ -127,7 +184,8 @@ def make_paper_table(data="src/data/papers.csv"):
                     ),
                 ]
             ),
-        ]
+        ],
+        # className="auto-resize-900",
     )
 
     return paper_table
