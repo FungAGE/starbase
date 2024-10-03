e526ddadbe1599ce3ec3cc87d57cff88000b9b24
AdrianForsythe <adrian.e.forsythe@gmail.com>
2024-10-03 11:10:56 +0200

    global variable for mounted directory

--- src/components/sqlite.py
+++ src/components/sqlite.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from sqlalchemy import create_engine
+from src.components.callbacks import MOUNTED_DIRECTORY_PATH
 
-engine = create_engine("sqlite:///database_folder/starbase.sqlite")
-
-query = "SELECT name FROM sqlite_master WHERE type='table'"
-sql_tbls = pd.read_sql_query(query, engine)
+if MOUNTED_DIRECTORY_PATH:
+  engine = create_engine(f"sqlite:///{MOUNTED_DIRECTORY_PATH}/starbase.sqlite")
+  query = "SELECT name FROM sqlite_master WHERE type='table'"
+  sql_tbls = pd.read_sql_query(query, engine)
