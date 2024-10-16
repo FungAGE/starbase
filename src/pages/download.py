e6f39673fcfcd77124af517e2992c48ae54f8bb3
AdrianForsythe <adrian.e.forsythe@gmail.com>
2024-10-16 11:34:30 +0200

    fixed download behaviour and improved table layout

--- src/pages/download.py
+++ src/pages/download.py
@@ -8,7 +8,6 @@ logging.basicConfig(level=logging.DEBUG)
 
 import dash
 from dash import html, dcc, dash_table, callback
-import dash_bootstrap_components as dbc
 import dash_mantine_components as dmc
 from dash.dependencies import Output, Input
 import pandas as pd
@@ -60,6 +59,11 @@ layout = dmc.Container(
     fluid=True,
     children=[
         dcc.Location(id="url", refresh=False),
+        dmc.Center(
+            children=[
+                dmc.Title("Choose Starships to Download:"),
+            ],
+        ),
         dmc.Grid(
             justify="center",
             align="center",
@@ -68,78 +72,86 @@ layout = dmc.Container(
             children=[
                 dmc.GridCol(
                     span=12,
-                    children=[dmc.Center([dmc.Title("Choose Starships to Download:")])],
-                ),
-                dmc.GridCol(
-                    span=12,
                     children=[
                         dmc.Center(
-                            [
-                                dbc.Button(
-                                    "Download All Starships",
-                                    id="download-all-btn",
-                                    class_name="text-custom text-custom-sm text-custom-md",
-                                ),
+                            children=[
+                                dmc.Stack(
+                                    children=[
+                                        html.Div(
+                                            [
+                                                dmc.NotificationProvider(),
+                                                html.Div(id="dl-notify"),
+                                                dmc.SimpleGrid(
+                                                    cols={"md": 2, "sm": 1},
+                                                    spacing={"base": 10, "sm": "xl"},
+                                                    verticalSpacing={
+                                                        "base": "md",
+                                                        "sm": "xl",
+                                                    },
+                                                    children=[
+                                                        dmc.Button(
+                                                            "Download All Starships",
+                                                            id="download-all-btn",
+                                                            className="text-custom text-custom-sm text-custom-md",
+                                                            style={"width": "100%"},
+                                                        ),
+                                                        dmc.Button(
+                                                            "Download Selected Starships",
+                                                            id="download-selected-btn",
+                                                            className="text-custom text-custom-sm text-custom-md",
+                                                            style={"width": "100%"},
+                                                        ),
+                                                    ],
+                                                ),
+                                            ]
+                                        ),
+                                        dcc.Download(id="dl-package"),
+                                        html.Div(
+                                            [
+                                                dash_table.DataTable(
+                                                    id="dl-table",
+                                                    data=[],
+                                                    columns=table_columns,
+                                                    selected_columns=[],
+                                                    selected_rows=[],
+                                                    editable=False,
+                                                    filter_action="native",
+                                                    sort_action="native",
+                                                    sort_mode="multi",
+                                                    row_selectable="multi",
+                                                    row_deletable=False,
+                                                    page_action="native",
+                                                    page_current=0,
+                                                    page_size=24,
+                                                    markdown_options={"html": True},
+                                                    style_table={
+                                                        "overflowX": "auto",
+                                                        "overflowY": "auto",
+                                                        "maxHeight": "400px",
+                                                    },
+                                                    style_data={
+                                                        "height": "auto",
+                                                        "whiteSpace": "minimal",
+                                                        "overflow": "hidden",
+                                                    },
+                                                    style_cell={
+                                                        "minWidth": "0px",
+                                                        "maxWidth": "300px",
+                                                        "whiteSpace": "minimal",
+                                                        "textAlign": "left",
+                                                    },
+                                                    style_header={
+                                                        "backgroundColor": "lightgrey",
+                                                        "fontWeight": "bold",
+                                                        "textAlign": "left",
+                                                    },
+                                                ),
+                                            ],
+                                        ),
+                                    ]
+                                )
                             ],
                         ),
-                    ],
-                ),
-                dmc.GridCol(
-                    span=12,
-                    children=[
-                        dmc.Center(
-                            [
-                                dbc.Button(
-                                    "Download Selected Starships",
-                                    id="download-selected-btn",
-                                    class_name="text-custom text-custom-sm text-custom-md",
-                                ),
-                            ]
-                        )
-                    ],
-                ),
-                dcc.Download(id="dl-package"),
-                html.Div(id="dl-warning"),
-                dmc.GridCol(
-                    span=10,
-                    children=[
-                        dmc.Center(
-                            [
-                                html.Div(
-                                    [
-                                        dash_table.DataTable(
-                                            id="dl-table",
-                                            data=[],
-                                            columns=table_columns,
-                                            selected_columns=[],
-                                            selected_rows=[],
-                                            editable=False,
-                                            filter_action="native",
-                                            sort_action="native",
-                                            sort_mode="multi",
-                                            row_selectable="multi",
-                                            row_deletable=False,
-                                            page_action="native",
-                                            page_current=0,
-                                            page_size=24,
-                                            markdown_options={"html": True},
-                                            style_table={
-                                                "overflowX": "auto",
-                                            },
-                                            style_data={
-                                                "whiteSpace": "minimal",
-                                            },
-                                            style_cell={
-                                                "minWidth": "100px",
-                                                "maxWidth": "100%",
-                                                "textAlign": "left",
-                                            },
-                                        ),
-                                    ],
-                                    className="auto-resize-900",
-                                )
-                            ]
-                        )
                     ],
                 ),
             ],
@@ -162,8 +174,8 @@ def make_dl_table(url):
         df = pd.read_sql_query(query, engine)
         if df.empty:
             logging.error("Query returned an empty DataFrame.")
-        else:
-            logging.info(f"Retrieved {len(df)} records from the database.")
+        # else:
+        # logging.info(f"Retrieved {len(df)} records from the database.")
 
         df.fillna("", inplace=True)  # Replace NaN with an empty string
         return df.to_dict("records")
@@ -173,93 +185,149 @@ def make_dl_table(url):
         return []
 
 
+def notification_base(title, message):
+    dmc.Notification(
+        title=title,
+        id="simple-notify",
+        action="show",
+        message=message,
+    )
+
+
 @callback(
-    [Output("dl-package", "data"), Output("dl-warning", "children")],
+    [
+        Output("dl-package", "data"),
+        Output("dl-notify", "children"),
+        Output("download-all-btn", "disabled"),
+        Output("download-selected-btn", "disabled"),
+    ],
     [
         Input("download-all-btn", "n_clicks"),
         Input("download-selected-btn", "n_clicks"),
+        Input("dl-table", "data"),
         Input("dl-table", "derived_virtual_selected_rows"),
-        Input("dl-table", "derived_virtual_selected_data"),
     ],
 )
-def generate_download(dl_all, dl_select, rows, data):
-    try:
-        if dl_all or dl_select:
-            logging.info(f"dl_all={dl_all}, dl_select={dl_select}, rows={rows}")
+def generate_download(dl_all, dl_select, table_data, selected_rows):
+    # logging.info(
+    #     f"dl_all={dl_all}, dl_select={dl_select}, table_data_length={len(table_data)}, selected_rows={selected_rows}"
+    # )
 
-            if dl_select and (rows is None or rows == []):
+    if not table_data or len(table_data) == 0:
+        logging.error("No data available from the table for processing.")
+        return (
+            dash.no_update,
+            notification_base(title="error", message="No data available for download"),
+            False,
+            False,
+        )
+
+    if not dl_all and not dl_select:
+        # logging.debug("No download action triggered. dl_all or dl_select not active.")
+        return dash.no_update, None, False, False
+
+    query = """
+    SELECT s.*, a.accession_tag
+    FROM ships s
+    LEFT JOIN accessions a ON s.accession = a.id
+    """
+    table_df = pd.DataFrame(table_data)
+    try:
+
+        if dl_all:
+            accessions = table_df["accession_tag"].to_list()
+            # logging.info("Using all table data for download.")
+
+            df = pd.read_sql_query(query, engine)
+
+        elif dl_select:
+            if not selected_rows or len(selected_rows) == 0:
                 logging.warning(
                     "Download selected was triggered but no rows are selected."
                 )
-                return None, dbc.Alert(
-                    "Make a selection in the table first", color="warning"
+                return (
+                    dash.no_update,
+                    notification_base(
+                        title="Warning:", message="Make a selection in the table first"
+                    ),
+                    False,
+                    False,
                 )
 
-            # Validate if data is available
-            if not data:
-                logging.error("No data available from the table for processing.")
-                return None, dbc.Alert("No data available for download", color="danger")
-
-            # Extract accessions safely
-            try:
-                accessions = pd.DataFrame(data).iloc[rows]["accession_tag"]
-                logging.info(f"Selected accessions: {accessions.tolist()}")
-            except Exception as e:
-                logging.error(f"Failed to extract accessions. Details: {e}")
-                return None, dbc.Alert(
-                    "Error extracting accessions from the selection", color="danger"
-                )
-
-            # Build and execute the SQL query
-            try:
-                query = """
-                SELECT s.*
-                FROM ships s
-                LEFT JOIN accessions a ON s.accession = a.id
-                """
-                if accessions is not None and len(accessions) > 0:
-                    placeholders = ",".join(["?"] * len(accessions))
-                    query += f" WHERE accession_tag IN ({placeholders})"
-                    df = pd.read_sql_query(query, engine, params=accessions.tolist())
-                    logging.info(f"Query returned {len(df)} records for accessions.")
+            else:
+                if isinstance(selected_rows, list) and all(
+                    isinstance(i, int) for i in selected_rows
+                ):
+                    selected_df = table_df.iloc[selected_rows]
                 else:
-                    df = pd.read_sql_query(query, engine)
-                    logging.info("Retrieved all records from the database.")
-            except Exception as e:
-                logging.error(f"Failed to execute database query. Details: {e}")
-                return None, dbc.Alert(
-                    "Error retrieving data from the database", color="danger"
-                )
+                    logging.error(
+                        "Invalid index type for iloc. Must be a list of integers."
+                    )
+
+                accessions = selected_df["accession_tag"].to_list()
+                # logging.info(f"Using selected table data: {accessions}")
+
+                placeholders = ",".join(["?"] * len(accessions))
+                query += f" WHERE a.accession_tag IN ({placeholders})"
+                params = [(acc,) for acc in accessions]  # List of single-element tuples
+                df = pd.read_sql_query(query, engine, params=params)
 
             if df.empty:
-                logging.warning("Query returned no matching records.")
-                return None, dbc.Alert(
-                    "No matching records found for the selected accessions",
-                    color="warning",
+                logging.error("No data available for selected rows.")
+                return (
+                    dash.no_update,
+                    notification_base(
+                        title="Error:", message="No data available for selected rows."
+                    ),
+                    False,
+                    False,
                 )
 
-            # Create FASTA content
+        if df.empty:
+            logging.warning("Query returned no matching records.")
+            return (
+                dash.no_update,
+                notification_base(
+                    title="Error:",
+                    message="No matching records found for the selected accessions",
+                ),
+                False,
+                False,
+            )
+        else:
+            # logging.info(f"Retrieved {len(df)} records from the database.")
             try:
                 fasta_content = [
                     f">{row['accession_tag']}\n{row['sequence']}"
                     for _, row in df.iterrows()
                 ]
                 fasta_str = "\n".join(fasta_content)
-                logging.info("FASTA content created successfully.")
-                return dcc.send_string(fasta_str, filename="starships.fasta"), None
+                # logging.info("FASTA content created successfully.")
+                return (
+                    dcc.send_string(fasta_str, filename="starships.fasta"),
+                    None,
+                    True,
+                    False,
+                )
             except Exception as e:
                 logging.error(f"Failed to create FASTA content. Details: {e}")
-                return None, dbc.Alert(
-                    "Error creating FASTA file for download", color="danger"
+                return (
+                    dash.no_update,
+                    notification_base(
+                        title="Error:",
+                        message="Error when creating FASTA file for download",
+                    ),
+                    False,
+                    False,
                 )
-        else:
-            logging.info("No download action triggered.")
-            return dash.no_update, None
-
     except Exception as e:
-        logging.error(
-            f"An unexpected error occurred in generate_download. Details: {e}"
-        )
-        return None, dbc.Alert(
-            "An unexpected error occurred during the download process", color="danger"
+        logging.error(f"Failed to execute database query. Details: {e}")
+        return (
+            dash.no_update,
+            notification_base(
+                title="Error:",
+                message=f"Failed to execute database query. Details: {e}",
+            ),
+            False,
+            False,
         )
