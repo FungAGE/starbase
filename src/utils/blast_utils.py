9c52be21a5ad93703e8be53273f973d54d96f96a
AdrianForsythe <adrian.e.forsythe@gmail.com>
2024-10-17 11:30:52 +0200

    moved blast stitching to blast utils

--- src/utils/blast_utils.py
+++ src/utils/blast_utils.py
@@ -4,7 +4,7 @@ warnings.filterwarnings("ignore")
 
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.ERROR)
 
 import dash_bootstrap_components as dbc
 from dash import dash_table, dcc, html
@@ -29,7 +29,6 @@ from Bio.Seq import Seq
 from Bio.SeqUtils import nt_search
 
 from src.utils.parsing import parse_fasta_from_file, parse_fasta_from_text, clean_shipID
-from src.utils.BLASTstitcher import stitch_blast
 
 
 def write_temp_fasta(header, sequence):
@@ -108,6 +107,194 @@ def guess_seq_type(query_seq):
         return None
 
 
+def print_table(list_thing, output_name, header=""):
+    with open(output_name, "w") as ofile:
+        if header != "":
+            ofile.write(header + "\n")
+        for line in list_thing:
+            hitstring = "\t".join(str(tab) for tab in line)
+            ofile.write(hitstring + "\n")
+
+
+def stitch_blast(tabfile, output_name):
+    # ------------------------------------------------------
+    # Read file into list
+    # ------------------------------------------------------
+    with open(tabfile, "r") as tabopen:
+        unsorted_tabs = [line.rstrip("\n").split("\t") for line in tabopen]
+
+    # ------------------------------------------------------
+    # Sort file in case some subjects have multiple hits in different places
+    # ------------------------------------------------------
+    # Make a list of all unique queries
+    queries = list(set(tab[0] for tab in unsorted_tabs))
+
+    # tabs = [] # the future sorted list
+    THRESHOLD = (
+        2500  # A threshold for the distance between two hits to be distinct loci
+    )
+    stitchedtab = []
+
+    # For every query, sort locally
+    for query in queries:
+        subject_dic = {}
+        right_subject_order = []  # To keep the right order
+        for tab in unsorted_tabs:
+            if query == tab[0]:  # Just for this query
+                subject = tab[1]
+                if subject not in subject_dic.keys():
+                    subject_dic[subject] = [tab]
+                    right_subject_order.extend([subject])
+                else:
+                    subject_dic[subject].append(tab)
+
+        # Sort the resulting list for that particular query, check every subject and sort it locally
+        for sub in right_subject_order:
+            currentsubject_sorted = sorted(
+                subject_dic[sub], key=lambda x: int(x[9])
+            )  # Sort by the subject_start
+            # tabs.extend(currentsubject_sorted) # Save it into the final tabs list
+
+            # ------------------------------------------------------
+            # Stitch pieces together
+            # ------------------------------------------------------
+            # print("Subject:", sub)
+
+            maxalign = 0
+            queryStarts = []
+            queryEnds = []
+            subjectStarts = []
+            subjectEnds = []
+
+            for i in range(0, len(currentsubject_sorted)):  # Find the main piece
+                # print(i, len(currentsubject_sorted), currentsubject_sorted[i] # For debugging)
+                # query_id, subject_id, percent_identity, alignment_length, N_mismatches, N_gaps, query_start, query_end, subject_start, subject_end, evalue, bit_score = currentsubject_sorted[i]
+                query_start = int(currentsubject_sorted[i][6])
+                query_end = int(currentsubject_sorted[i][7])
+                query_seq = currentsubject_sorted[i][12]
+
+                alignment_length = int(currentsubject_sorted[i][3])
+                subject_start = int(currentsubject_sorted[i][8])
+                subject_end = int(currentsubject_sorted[i][9])
+                subject_seq = currentsubject_sorted[i][13]
+
+                queryStarts.append(query_start)
+                queryEnds.append(query_end)
+                subjectStarts.append(subject_start)
+                subjectEnds.append(subject_end)
+
+                # Is this last piece one better than the previous ones?
+                if alignment_length > maxalign:
+                    upper_hit = currentsubject_sorted[i]
+                    maxalign = alignment_length
+
+                # There is only one clean hit
+                if len(currentsubject_sorted) == 1:
+                    stitchedtab.append(currentsubject_sorted[i])
+
+                # The hit is broken
+                elif i < len(currentsubject_sorted) - 1:
+                    next_subject_start = int(currentsubject_sorted[i + 1][8])
+
+                    if (
+                        abs(subject_end - next_subject_start) > THRESHOLD
+                    ):  # It's probably not part of the same hit # It was subject_start - next_subject_start before
+                        # So write down the previous one
+                        # -----------------
+                        # The new values for the subject
+                        # -----------------
+                        # query_id, subject_id, percent_identity, alignment_length, N_mismatches, N_gaps, query_start, query_end, subject_start, subject_end, evalue, bit_score
+                        new_tab = upper_hit[0:4]
+
+                        # These are our new values of the "unbroken" hit, but I'm not sure how to retrieve the no. of gaps and mistmatches
+                        new_tab.extend([".", ".", min(queryStarts), max(queryEnds)])
+
+                        # subject_start > subject_end for the upper_hit
+                        if int(upper_hit[8]) > int(upper_hit[9]):  # hit is reversed
+                            new_tab.extend([max(subjectStarts), min(subjectEnds)])
+                        else:
+                            new_tab.extend([min(subjectStarts), max(subjectEnds)])
+
+                        # Let's leave the e-val and Bit score the same as the upper hit
+                        new_tab.extend(upper_hit[10:11])
+
+                        gap_length = abs(next_subject_start - subject_end)
+
+                        # Concatenate subject sequences with '-' characters filling the gap
+                        stitched_subject_seq = (
+                            currentsubject_sorted[i][13]
+                            + "-" * gap_length
+                            + currentsubject_sorted[i + 1][13]
+                        )
+
+                        # Update the subject_seq in the upper_hit
+                        upper_hit[13] = stitched_subject_seq
+
+                        stitchedtab.append(new_tab)  # Write it in the final output
+
+                        # -----------------
+                        # Reset for the next hit
+                        # -----------------
+                        queryStarts = []
+                        queryEnds = []
+                        subjectStarts = []
+                        subjectEnds = []
+
+                        maxalign = int(currentsubject_sorted[i + 1][3])
+                        upper_hit = currentsubject_sorted[i + 1]
+
+                else:
+                    # The last hit in that subject
+                    # -----------------
+                    # The new values for the subject
+                    # -----------------
+                    new_tab = upper_hit[0:4]
+
+                    # These are our new values of the "unbroken" hit, but I'm not sure how to retrieve the no. of gaps and mistmatches
+                    new_tab.extend([".", ".", min(queryStarts), max(queryEnds)])
+
+                    # subject_start > subject_end for the upper_hit
+                    if int(upper_hit[8]) > int(upper_hit[9]):  # hit is reversed
+                        new_tab.extend([max(subjectStarts), min(subjectEnds)])
+                    else:
+                        new_tab.extend([min(subjectStarts), max(subjectEnds)])
+                    # Let's leave the e-val and Bit score the same as the upper hit
+                    new_tab.extend(upper_hit[10:])
+
+                    stitchedtab.append(new_tab)  # Write it in the final output
+
+            # print # For separating the subjects
+
+    # ------------------------------------------------------
+    # Print filtered tab file
+    # ------------------------------------------------------
+    print_table(stitchedtab, output_name)
+    df = pd.read_csv(
+        output_name,
+        sep="\t",
+        names=[
+            "qseqid",
+            "sseqid",
+            "pident",
+            "length",
+            "mismatch",
+            "gapopen",
+            "qstart",
+            "qend",
+            "sstart",
+            "send",
+            "evalue",
+            "bitscore",
+            "qseq",
+            "sseq",
+        ],
+    )
+    df = df.dropna()
+
+    logging.info(f"BLAST results parsed with {len(df)} hits.")
+    return df
+
+
 def run_blast(
     db_list=None,
     query_type=None,
@@ -180,11 +367,11 @@ def run_blast(
 
         # with open(tmp_query_fasta, "r") as file:
         #     file_contents = file.read()
-        # logging.info(file_contents)
+        logging.info(file_contents)
 
         # with open(tmp_blast, "r") as file:
         #     file_contents = file.read()
-        # logging.info(file_contents)
+        logging.info(file_contents)
 
         return df
     except Exception as e:
@@ -298,7 +485,7 @@ def extract_gene_from_hmmer(parsed_file):
 
     # top_hit_out_path = tempfile.NamedTemporaryFile(suffix=".besthit.txt").name
     # top_hit_out_path = tempfile.NamedTemporaryFile(suffix=".best_hsp.fa").name
-    # logging.info(f"Best hit for gene sequence: {top_hit_out_path}")
+    logging.info(f"Best hit for gene sequence: {top_hit_out_path}")
 
     query = min_evalue_rows.loc[0, "query_id"]
     qseq = min_evalue_rows.loc[0, "query_seq"].replace(".", "")
