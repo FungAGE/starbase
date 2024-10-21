ba1fd64234b1be73b01ee06c7dd1c9736abac25b
AdrianForsythe <adrian.e.forsythe@gmail.com>
2024-10-21 15:00:38 +0200

    incorporating models with alembic

--- src/components/sqlite.py
+++ src/components/sqlite.py
@@ -1,7 +1,18 @@
-import pandas as pd
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base
 
-engine = None
-
+# Connect to the SQLite database file
+DATABASE_URL = "sqlite:///src/data/db/starbase.sqlite"
 # Initialize the SQLite engine
-engine = create_engine("sqlite:///src/data/db/starbase.sqlite")
+engine = create_engine(DATABASE_URL)
+
+# Bind the models (Base) to the engine
+Base.metadata.bind = engine
+
+# Create all tables in the database (if they don't exist)
+Base.metadata.create_all(engine)
+
+# Create a session factory
+Session = sessionmaker(bind=engine)
+session = Session()
