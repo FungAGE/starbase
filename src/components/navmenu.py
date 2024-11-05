758349ca6d404e5db93045800d35173a2403d267
AdrianForsythe <adrian.e.forsythe@gmail.com>
2024-11-05 12:28:10 +0100

    disabled features if sql is not connected

--- src/components/navmenu.py
+++ src/components/navmenu.py
@@ -13,101 +13,105 @@ from src.pages import (
 )
 
 
-navbar_content = [
-    dbc.NavItem(
-        dbc.NavLink(
-            html.P(
-                ["Download ", html.Span("starbase", className="logo-text")],
-                className="nav-item-text",
-            ),
-            href=DL_URL,
-            active="exact",
-            className="nav-item-link",
-        )
-    ),
-    dbc.NavItem(
-        dbc.NavLink(
-            html.P(
-                [
-                    "Wiki",
-                ],
-                className="nav-item-text",
-            ),
-            href=WIKI_URL,
-            active="exact",
-            className="nav-item-link",
-        )
-    ),
-    # dbc.NavItem(
-    #     dbc.NavLink(
-    #         html.P(
-    #             ["Explore"],
-    #             className="nav-item-text",
-    #         ),
-    #         href=EXPLORE_URL,
-    #         active="exact",
-    #         className="nav-item-link",
-    #     )
-    # ),
-    dbc.NavItem(
-        dbc.NavLink(
-            html.P(
-                ["Starship Viewer"],
-                className="nav-item-text",
-            ),
-            href=PGV_URL,
-            active="exact",
-            className="nav-item-link",
-        )
-    ),
-    dbc.NavItem(
-        dbc.NavLink(
-            html.P(
-                ["BLAST"],
-                className="nav-item-text",
-            ),
-            href=BLAST_URL,
-            active="exact",
-            className="nav-item-link",
-        )
-    ),
-    dbc.NavItem(
-        dbc.NavLink(
-            html.P(
-                ["Submit"],
-                className="nav-item-text",
-            ),
-            href=SUBMIT_URL,
-            active="exact",
-            className="nav-item-link",
-        )
-    ),
-    dbc.NavItem(
-        dbc.NavLink(
-            html.P(["About"], className="nav-item-text"),
-            href=ABOUT_URL,
-            active="exact",
-            className="nav-item-link",
-        )
-    ),
-]
+def navmenu(buttons_disabled=False):
+    navbar_content = [
+        dbc.NavItem(
+            dbc.NavLink(
+                html.P(
+                    ["Download ", html.Span("starbase", className="logo-text")],
+                    className="nav-item-text",
+                ),
+                href=DL_URL,
+                active="exact",
+                className="nav-item-link",
+                disabled=buttons_disabled,
+            )
+        ),
+        dbc.NavItem(
+            dbc.NavLink(
+                html.P(
+                    [
+                        "Wiki",
+                    ],
+                    className="nav-item-text",
+                ),
+                href=WIKI_URL,
+                active="exact",
+                className="nav-item-link",
+                disabled=buttons_disabled,
+            )
+        ),
+        # dbc.NavItem(
+        #     dbc.NavLink(
+        #         html.P(
+        #             ["Explore"],
+        #             className="nav-item-text",
+        #         ),
+        #         href=EXPLORE_URL,
+        #         active="exact",
+        #         className="nav-item-link",disabled=buttons_disabled
+        #     )
+        # ),
+        dbc.NavItem(
+            dbc.NavLink(
+                html.P(
+                    ["Starship Viewer"],
+                    className="nav-item-text",
+                ),
+                href=PGV_URL,
+                active="exact",
+                className="nav-item-link",
+                disabled=buttons_disabled,
+            )
+        ),
+        dbc.NavItem(
+            dbc.NavLink(
+                html.P(
+                    ["BLAST"],
+                    className="nav-item-text",
+                ),
+                href=BLAST_URL,
+                active="exact",
+                className="nav-item-link",
+                disabled=buttons_disabled,
+            )
+        ),
+        dbc.NavItem(
+            dbc.NavLink(
+                html.P(
+                    ["Submit"],
+                    className="nav-item-text",
+                ),
+                href=SUBMIT_URL,
+                active="exact",
+                className="nav-item-link",
+                disabled=buttons_disabled,
+            )
+        ),
+        dbc.NavItem(
+            dbc.NavLink(
+                html.P(["About"], className="nav-item-text"),
+                href=ABOUT_URL,
+                active="exact",
+                className="nav-item-link",
+                disabled=buttons_disabled,
+            )
+        ),
+    ]
 
+    navbar = dbc.NavbarSimple(
+        children=navbar_content,
+        brand=html.Span("starbase", className="logo-text"),
+        brand_href=HOME_URL,
+        brand_style={
+            "align-items": "center",
+            "justify-content": "center",
+            "textAlign": "left",
+        },
+        color="primary",
+        dark=True,
+    )
 
-navbar = dbc.NavbarSimple(
-    children=navbar_content,
-    brand=html.Span("starbase", className="logo-text"),
-    brand_href=HOME_URL,
-    brand_style={
-        "align-items": "center",
-        "justify-content": "center",
-        "textAlign": "left",
-    },
-    color="primary",
-    dark=True,
-)
-
-
-def navmenu():
     return html.Div(
         [navbar],
     )
