0223d3eed666394b78becdf3368d7f367e5fe043
AdrianForsythe <adrian.e.forsythe@gmail.com>
2024-11-05 12:00:16 +0100

    added modal to blast table

--- src/pages/wiki.py
+++ src/pages/wiki.py
@@ -23,10 +23,10 @@ from src.components.sql_queries import (
     cache_sunburst_plot,
     fetch_paper_data,
 )
-from src.components.mariadb import engine
 from src.components.tables import make_ship_table
 from src.utils.plot_utils import make_logo
 from src.utils.parsing import clean_contigIDs
+from src.components.callbacks import create_accession_modal
 
 dash.register_page(__name__)
 
@@ -147,10 +147,10 @@ def create_accordion_item(df, papers, category):
 
 modal = dbc.Modal(
     [
-        dbc.ModalHeader(dbc.ModalTitle(id="modal-title")),
-        dbc.ModalBody(id="modal-content"),
+        dbc.ModalHeader(dbc.ModalTitle(id="wiki-modal-title")),
+        dbc.ModalBody(id="wiki-modal-content"),
     ],
-    id="modal",
+    id="wiki-modal",
     is_open=False,
 )
 
@@ -405,140 +405,12 @@ def create_sidebar(active_item, cached_meta):
         raise
 
 
-def create_accession_modal(accession):
-    # import tempfile
-    # from src.pages.pgv import load_fa, write_tmp, load_gff, single_pgv
-
-    initial_df = load_from_cache("meta_data")
-    modal_data = initial_df[initial_df["accession_tag"] == accession]
-
-    modal_title = html.H2(f"Ship Accession: {accession}")
-
-    # tmp_pgv = tempfile.NamedTemporaryFile(suffix=".html", delete=True).name
-
-    # with tempfile.TemporaryDirectory() as temp_dir:
-    #     tmp_gffs = []
-    #     tmp_fas = []
-    #     for index, row in modal_data.iterrows():
-    #         logger.info(f"Fetching FA for accession: {accession}")
-    #         fa_df = load_fa(accession)
-    #         tmp_fa = write_tmp(fa_df, accession, "fa", temp_dir)
-    #         tmp_fas.append(str(tmp_fa))
-
-    #         logger.info(f"Fetching GFF for accession: {accession}")
-    #         gff_df = load_gff(accession)
-
-    #         tmp_gff = write_tmp(gff_df, accession, "gff", temp_dir)
-    #         tmp_gffs.append(tmp_gff)
-
-    #         output = html.P("Select up to four Starships to compare.")
-    #     single_pgv(tmp_gffs[0], tmp_pgv)
-    #     try:
-    #         with open(tmp_pgv, "r") as file:
-    #             pgv_content = file.read()
-    #     except IOError:
-    #         output = html.P("Failed to read the temporary file.")
-
-    #     output = html.Iframe(
-    #         srcDoc=pgv_content,
-    #         style={
-    #             "width": "100%",
-    #             "height": "100%",
-    #             "border": "none",
-    #         },
-    #     )
-
-    # modal_content = output
-
-    modal_content = html.Div(
-        [
-            html.Div(
-                [html.Strong("ship_id: "), html.Span(modal_data["ship_id"].iloc[0])]
-            ),
-            html.Div(
-                [
-                    html.Strong("curated_status: "),
-                    html.Span(modal_data["curated_status"].iloc[0]),
-                ]
-            ),
-            html.Div(
-                [
-                    html.Strong("Number of genomes with ship present:"),
-                    html.Span(len(modal_data)),
-                ]
-            ),
-            html.Hr(),
-            # TODO: update genome accessions field
-            html.Div(
-                [
-                    html.Strong("Assembly Accession(s): "),
-                    html.Span(""),
-                ]
-            ),
-            html.Div(
-                [
-                    html.Strong("Genome Source: "),
-                    html.Span(modal_data["genomeSource"].iloc[0]),
-                ]
-            ),
-            html.Div(
-                [
-                    html.Strong("Citation: "),
-                    html.Span(modal_data["citation"].iloc[0]),
-                ]
-            ),
-            html.Div(
-                [html.Strong("contigID: "), html.Span(modal_data["contigID"].iloc[0])]
-            ),
-            html.Div(
-                [
-                    html.Strong("elementBegin: "),
-                    html.Span(modal_data["elementBegin"].iloc[0]),
-                ]
-            ),
-            html.Div(
-                [
-                    html.Strong("elementEnd: "),
-                    html.Span(modal_data["elementEnd"].iloc[0]),
-                ]
-            ),
-            html.Div([html.Strong("size: "), html.Span(modal_data["size"].iloc[0])]),
-            html.Hr(),
-            html.Div([html.Strong("order: "), html.Span(modal_data["order"].iloc[0])]),
-            html.Div(
-                [html.Strong("family: "), html.Span(modal_data["family"].iloc[0])]
-            ),
-            html.Div(
-                [html.Strong("species: "), html.Span(modal_data["species"].iloc[0])]
-            ),
-            html.Div(
-                [html.Strong("strain: "), html.Span(modal_data["strain"].iloc[0])]
-            ),
-            html.Div(
-                [
-                    html.Strong("taxid: "),
-                    html.A(
-                        (
-                            int(modal_data["taxid"].iloc[0])
-                            if isinstance(modal_data["taxid"].iloc[0], (float, int))
-                            else modal_data["taxid"].iloc[0]
-                        ),
-                        href=f"https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id={int(modal_data['taxid'].iloc[0]) if isinstance(modal_data['taxid'].iloc[0], (float, int)) else modal_data['taxid'].iloc[0]}",
-                    ),
-                ]
-            ),
-        ]
-    )
-
-    return modal_content, modal_title
-
-
 @callback(
-    Output("modal", "is_open"),
-    Output("modal-content", "children"),
-    Output("modal-title", "children"),
+    Output("wiki-modal", "is_open"),
+    Output("wiki-modal-content", "children"),
+    Output("wiki-modal-title", "children"),
     Input("wiki-table", "active_cell"),
-    State("modal", "is_open"),
+    State("wiki-modal", "is_open"),
     State("wiki-table", "data"),
 )
 def toggle_modal(active_cell, is_open, table_data):
