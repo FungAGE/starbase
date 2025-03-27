ce1e61df30dcfc228ddfbc8e7637d82114175f10
AdrianForsythe <adrian.e.forsythe@gmail.com>
2025-03-27 14:45:22 +0100

    update path for tar script

--- src/database/tar-db.sh
+++ src/database/tar-db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd  || exit
-tar -cf db.tar db/captain/tyr/faa/blastdb db/captain/tyr/faa/hmm/combined.hmm db/captain/tyr/fna/hmm/combined.hmm db/ships/fna/blastdb db/starbase.sqlite db/submissions.sqlite
+cd src/database || exit
+tar -cf db.tar db/captain/tyr/faa/blastdb db/captain/tyr/faa/hmm/combined.hmm db/captain/tyr/fna/hmm/combined.hmm db/ships/fna/blastdb db/starbase.sqlite db/submissions.sqlite db/telemetry.sqlite
 gzip -f db.tar
