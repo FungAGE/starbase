a7390cbaa6a49edcfc1a846afade12db211d697b
j-awada <jana.awada@scilifelab.uu.se>
2025-04-16 16:22:22 +0200

    comment cleanup

--- src/tests/test_submission.py
+++ src/tests/test_submission.py
@@ -3,7 +3,6 @@ from models import Accession
 
 def test_create_new_accession(db_session):
     """Test creating and saving a new accession record"""
-    # Create a new accession record
     new_accession = Accession(
         ship_name="Enterprise",
         accession="123456",
@@ -11,11 +10,9 @@ def test_create_new_accession(db_session):
         accession_new=1,
     )
 
-    # Add and commit the new accession to the database
     db_session.add(new_accession)
     db_session.commit()
 
-    # Verify the accession was created
     saved_accession = db_session.query(Accession).filter_by(accession="123456").first()
     assert saved_accession is not None
     assert saved_accession.ship_name == "Enterprise"
