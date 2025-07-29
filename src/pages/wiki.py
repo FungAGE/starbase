20932e2bc0060a8a65bceb978f6825312a677e6d
AdrianForsythe <adrian.e.forsythe@gmail.com>
2025-07-29 11:46:37 +0200

    fixing fasta headers for download

--- src/pages/wiki.py
+++ src/pages/wiki.py
@@ -989,10 +989,10 @@ def generate_download_helper(rows, curated, dereplicate):
                 # Full header for single entries
                 header = create_ncbi_style_header(row)
             if header is None or header == "None":
-                header = f">{row['accession_tag']}" + (
+                header = f"{row['accession_tag']}" + (
                     f" [family={row['familyName']}]" if row.get("familyName") else ""
                 )
-            fasta_content.append(f"{header}\n{row['sequence']}")
+            fasta_content.append(f">{header}\n{row['sequence']}")
 
         fasta_str = "\n".join(fasta_content)
         logger.debug(
