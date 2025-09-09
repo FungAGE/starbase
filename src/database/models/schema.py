fad20a8e1cdc22205a01e8db61145ad1fd32bc5f
forsythe <adrian.forsythe@ebc.uu.se>
2025-09-09 10:52:14 +0200

    updating incorrect schema

--- src/database/models/schema.py
+++ src/database/models/schema.py
@@ -6,6 +6,7 @@ from sqlalchemy import (
     String,
     Table,
     VARCHAR,
+    DateTime,
 )
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
@@ -27,13 +28,12 @@ class Accessions(Base):
     id = Column(Integer, primary_key=True)
     ship_name = Column(String)
     accession_tag = Column(String, unique=True)
-    version_tag = Column(String)
+    version_tag = Column(Integer)
 
     # Relationships
     ships = relationship("Ships", back_populates="accession_obj")
     gff_entries = relationship("Gff", back_populates="accession_obj")
-    joined_ships = relationship("JoinedShips", back_populates="accession_obj")
-    captains = relationship("Captains", back_populates="ship")
+    starship_features = relationship("StarshipFeatures", back_populates="accession")
 
 
 class Ships(Base):
@@ -41,10 +41,16 @@ class Ships(Base):
     id = Column(Integer, primary_key=True)
     sequence = Column(String)
     md5 = Column(String)
+    sequence_length = Column(Integer)
+    header = Column(String)
     accession_id = Column(Integer, ForeignKey("accessions.id"))
+    rev_comp_md5 = Column(String)
 
     # Relationships
     accession_obj = relationship("Accessions", back_populates="ships")
+    starship_features = relationship("StarshipFeatures", back_populates="ship")
+    joined_ships = relationship("JoinedShips", back_populates="ship")
+    captains = relationship("Captains", back_populates="ship")
 
 
 class Captains(Base):
@@ -52,44 +58,45 @@ class Captains(Base):
     id = Column(Integer, primary_key=True)
     captainID = Column(String, unique=True)
     sequence = Column(String)
-    ship_id = Column(Integer, ForeignKey("accessions.id"))
+    ship_id = Column(Integer, ForeignKey("ships.id"))
     reviewed = Column(String)
+    evidence = Column(String)
 
     # Relationships
-    ship = relationship("Accessions", back_populates="captains")
+    ship = relationship("Ships", back_populates="captains")
     features = relationship("StarshipFeatures", back_populates="captain")
 
 
 class Genome(Base):
     __tablename__ = "genomes"
-
     id = Column(Integer, primary_key=True)
     ome = Column(String(50))
-    taxonomy_id = Column(Integer, ForeignKey("taxonomy.id"))
     version = Column(String(50))
     genomeSource = Column(String(50))
     citation = Column(String(50))
     biosample = Column(String(50))
     acquisition_date = Column(Integer)
+    taxonomy_id = Column(Integer, ForeignKey("taxonomy.id"))
 
     taxonomy = relationship("Taxonomy", back_populates="genomes")
-
 
 class StarshipFeatures(Base):
     __tablename__ = "starship_features"
     id = Column(Integer, primary_key=True)
+    accession_id = Column(Integer, ForeignKey("accessions.id"))
+    ship_id = Column(Integer, ForeignKey("ships.id"))
     contigID = Column(String)
     starshipID = Column(String)
     captainID = Column(String)
-    elementBegin = Column(String)
-    elementEnd = Column(String)
-    elementLength = Column(String)
+    elementBegin = Column(Integer)
+    elementEnd = Column(Integer)
+    elementLength = Column(Integer)
     strand = Column(String)
     boundaryType = Column(String)
     emptySiteID = Column(String)
     emptyContig = Column(String)
-    emptyBegin = Column(String)
-    emptyEnd = Column(String)
+    emptyBegin = Column(Integer)
+    emptyEnd = Column(Integer)
     emptySeq = Column(String)
     upDR = Column(String)
     downDR = Column(String)
@@ -99,12 +106,12 @@ class StarshipFeatures(Base):
     TIRedit = Column(String)
     nestedInside = Column(String)
     containNested = Column(String)
-    ship_id = Column(Integer, ForeignKey("accessions.id"))
     captain_id = Column(Integer, ForeignKey("captains.id"))
 
     # Relationships
     accession = relationship("Accessions", back_populates="starship_features")
     captain = relationship("Captains", back_populates="features")
+    ship = relationship("Ships", back_populates="starship_features")
 
 
 class Papers(Base):
@@ -122,7 +129,7 @@ class Papers(Base):
     Date = Column(String)
     starshipMentioned = Column(String)
     typePaper = Column(String)
-    shortCitation = Column(VARCHAR)
+    shortCitation = Column(String)
 
     # Relationships
     family_names = relationship(
@@ -133,14 +140,14 @@ class Papers(Base):
 class FamilyNames(Base):
     __tablename__ = "family_names"
     id = Column(Integer, primary_key=True)
-    longFamilyID = Column(VARCHAR)
-    oldFamilyID = Column(VARCHAR)
+    longFamilyID = Column(String)
+    oldFamilyID = Column(String)
     clade = Column(Integer)
     newFamilyID = Column(Integer)
-    familyName = Column(VARCHAR)
-    type_element_reference = Column(VARCHAR)
-    notes = Column(VARCHAR)
-    otherFamilyID = Column(VARCHAR)
+    familyName = Column(String)
+    type_element_reference = Column(String)
+    notes = Column(String)
+    otherFamilyID = Column(String)
     paper_id = Column(Integer, ForeignKey("papers.id"))
 
     # Relationships
@@ -152,42 +159,36 @@ class FamilyNames(Base):
 class Taxonomy(Base):
     __tablename__ = "taxonomy"
     id = Column(Integer, primary_key=True)
-    name = Column(VARCHAR)
-    taxID = Column(VARCHAR)
-    superkingdom = Column(VARCHAR)
-    clade = Column(VARCHAR)
-    kingdom = Column(VARCHAR)
-    subkingdom = Column(VARCHAR)
-    phylum = Column(VARCHAR)
-    subphylum = Column(VARCHAR)
+    name = Column(String)
+    taxID = Column(String)
+    superkingdom = Column(String)
+    clade = Column(String)
+    kingdom = Column(String)
+    subkingdom = Column(String)
+    phylum = Column(String)
+    subphylum = Column(String)
     class_ = Column(
-        VARCHAR, name="class"
+        String, name="class"
     )  # Using class_ as class is a reserved keyword
-    subclass = Column(VARCHAR)
-    order = Column(VARCHAR)
-    suborder = Column(VARCHAR)
-    family = Column(VARCHAR)
-    genus = Column(VARCHAR)
-    species = Column(VARCHAR)
-    section = Column(VARCHAR)
-
-
-class NavisHaplotype(Base):
-    __tablename__ = "navis_haplotype"
-    id = Column(Integer, primary_key=True)
-    navis_name = Column(String)
-    haplotype_name = Column(String)
-    ship_family_id = Column(Integer, ForeignKey("family_names.id"))
+    subclass = Column(String)
+    order = Column(String)
+    suborder = Column(String)
+    family = Column(String)
+    genus = Column(String)
+    species = Column(String)
+    section = Column(String)
+    species_group = Column(String)
+    subgenus = Column(String)
+    strain = Column(String)
 
     # Relationships
-    family = relationship("FamilyNames")
+    genomes = relationship("Genome", back_populates="taxonomy")
 
 
 class Gff(Base):
     __tablename__ = "gff"
     id = Column(Integer, primary_key=True)
-    contigID = Column(String)
-    accession = Column(String)
+    accession_id = Column(Integer, ForeignKey("accessions.id"))
     source = Column(String)
     type = Column(String)
     start = Column(Integer)
@@ -196,56 +197,60 @@ class Gff(Base):
     strand = Column(String)
     score = Column(String)
     attributes = Column(String)
-    ship_id = Column(Integer, ForeignKey("accessions.id"))
+    ship_id = Column(Integer, ForeignKey("ships.id"))
+
+    # Relationships
+    accession_obj = relationship("Accessions", back_populates="gff_entries")
 
 
 class JoinedShips(Base):
     __tablename__ = "joined_ships"
     id = Column(Integer, primary_key=True)
     starshipID = Column(String)
-    genus = Column(String)
-    species = Column(String)
-    strain = Column(String)
     evidence = Column(String)
     source = Column(String)
-    contigID = Column(String)
-    captainID = Column(String)
-    elementBegin = Column(Integer)
-    elementEnd = Column(Integer)
-    size = Column(Integer)
-    strand = Column(String)
-    boundaryType = Column(String)
-    emptySiteID = Column(String)
-    emptyContig = Column(String)
-    emptyBegin = Column(Integer)
-    emptyEnd = Column(Integer)
-    emptySeq = Column(String)
-    upDR = Column(String)
-    downDR = Column(String)
-    DRedit = Column(String)
-    upTIR = Column(String)
-    downTIR = Column(String)
-    TIRedit = Column(String)
-    nestedInside = Column(String)
-    containNested = Column(String)
-    dr = Column(String)
-    tir = Column(String)
-    navis_name = Column(String)
-    haplotype_name = Column(String)
-    target = Column(String)
-    spok = Column(String)
-    ars = Column(String)
-    other = Column(String)
-    hgt = Column(String)
-    ship_family_id = Column(Integer, ForeignKey("family_names.id"))
     curated_status = Column(String)
-    taxid = Column(Integer)
-    ship_id = Column(Integer, ForeignKey("accessions.id"))
-    genome_id = Column(String)
-    ome = Column(String)
-    orphan = Column(String)
-    captainID_new = Column(Integer)
+    ship_family_id = Column(Integer, ForeignKey("family_names.id"))
+    tax_id = Column(Integer, ForeignKey("taxonomy.id"))
+    ship_id = Column(Integer, ForeignKey("ships.id"))
+    genome_id = Column(Integer, ForeignKey("genomes.id"))
+    captain_id = Column(Integer, ForeignKey("captains.id"))
+    ship_navis_id = Column(Integer, ForeignKey("navis_names.id"))
+    ship_haplotype_id = Column(Integer, ForeignKey("haplotype_names.id"))
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
 
     # Relationships
-    accession_obj = relationship("Accessions", back_populates="joined_ships")
     family = relationship("FamilyNames")
+    taxonomy = relationship("Taxonomy")
+    ship = relationship("Ships", back_populates="joined_ships")
+    genome = relationship("Genome")
+    captain = relationship("Captains")
+    navis = relationship("NavisNames")
+    haplotype = relationship("HaplotypeNames")
+
+
+class NavisNames(Base):
+    __tablename__ = "navis_names"
+    id = Column(Integer, primary_key=True)
+    navis_name = Column(String)
+    previous_navis_name = Column(String)
+    ship_family_id = Column(Integer, ForeignKey("family_names.id"))
+
+    # Relationships
+    family = relationship("FamilyNames")
+    haplotypes = relationship("HaplotypeNames", back_populates="navis")
+    joined_ships = relationship("JoinedShips", back_populates="navis")
+
+class HaplotypeNames(Base):
+    __tablename__ = "haplotype_names"
+    id = Column(Integer, primary_key=True)
+    haplotype_name = Column(String)
+    previous_haplotype_name = Column(String)
+    navis_id = Column(Integer, ForeignKey("navis_names.id"))
+    ship_family_id = Column(Integer, ForeignKey("family_names.id"))
+
+    # Relationships
+    family = relationship("FamilyNames")
+    navis = relationship("NavisNames", back_populates="haplotypes")
+    joined_ships = relationship("JoinedShips", back_populates="haplotype")
