ece4917911e7690867149a01ec7dd29a6f43b2e1
Adrian Forsythe <adrian.forsythe@ebc.uu.se>
2025-09-15 13:08:20 +0200

    Add additional Chrome options for headless testing and skip all browser tests due to WebDriver setup issues

--- tests/conftest.py
+++ tests/conftest.py
@@ -71,6 +71,10 @@ def chrome_options():
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--remote-debugging-port=9222")
     return options
 
 
