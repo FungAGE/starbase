96f0e89032e1f6582649c58b2dbf779408ee7058
Adrian Forsythe <adrian.forsythe@ebc.uu.se>
2025-11-19 13:52:56 +0100

    Remove worker-class option from uvicorn command
    Removed the worker class specification from the uvicorn command.

--- start-script.sh
+++ start-script.sh
@@ -64,7 +64,6 @@ else
     uvicorn --host=0.0.0.0 \
         --port=8000 \
         --workers=4 \
-        --worker-class=uvicorn.workers.UvicornWorker \
         --worker-timeout=300 \
         --max-requests=1000 \
         --max-requests-jitter=50 \
