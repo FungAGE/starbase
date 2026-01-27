fd3e0940ce907fd4316e1843c92a6c17951510a6
AdrianForsythe <adrian.forsythe@ebc.uu.se>
2026-01-28 00:24:40 +0100

    update modal tests

--- tests/api/test_routes.py
+++ tests/api/test_routes.py
@@ -2,38 +2,36 @@ import pytest
 from unittest.mock import patch
 
 
-def test_get_ship_accession_details(test_client):
-    """Test the /api/ship_accession_details/<ship_accession_id> route."""
-    ship_accession_id = "SSA002851"
-
-    with patch("src.api.routes.create_ship_accession_modal_data") as mock_create_modal_data:
-        mock_modal_data = {
-            "title": ship_accession_id,
-            "familyName": "TestFamily",
-            "curated_status": "curated"
-        }
-        mock_create_modal_data.return_value = mock_modal_data
-
-        response = test_client.get(f"/api/ship_accession_details/{ship_accession_id}")
-
-        assert response.status_code == 200
-        assert response.json == mock_modal_data
-        mock_create_modal_data.assert_called_once_with(ship_accession_id)
-
-
 def test_get_accession_details(test_client):
     """Test the /api/accession_details/<accession_id> route."""
     accession_id = "SSA002851"
+    ship_accession_id = "SSB000339"
 
     with patch("src.api.routes.create_accession_modal_data") as mock_create_modal_data:
         mock_modal_data = {
             "title": f"Starship Accession: {accession_id}",
             "familyName": "TestFamily",
-            "genomes_present": "1"
+            "genomes_present": "1",
         }
         mock_create_modal_data.return_value = mock_modal_data
 
         response = test_client.get(f"/api/accession_details/{accession_id}")
+
+        assert response.status_code == 200
+        assert response.json == mock_modal_data
+        mock_create_modal_data.assert_called_once_with(accession_id)
+
+    with patch(
+        "src.api.routes.create_ship_accession_modal_data"
+    ) as mock_create_modal_data:
+        mock_modal_data = {
+            "title": f"Starship Accession: {ship_accession_id}",
+            "familyName": "TestFamily",
+            "genomes_present": "1",
+        }
+        mock_create_modal_data.return_value = mock_modal_data
+
+        response = test_client.get(f"/api/accession_details/{ship_accession_id}")
 
         assert response.status_code == 200
         assert response.json == mock_modal_data
